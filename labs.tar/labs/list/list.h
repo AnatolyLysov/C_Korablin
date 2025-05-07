be734bae4c34bc41a8285bb332f8206c70f11315
Anatoly <56615869+AnatolyLysov@users.noreply.github.com>
2025-05-07 17:36:42 +0300

    initial commit

---
+++ labs.tar/labs/list/list.h
@@ -0,0 +1,13 @@
+
+struct tagItem {
+	void* p;
+	struct tagItem* prev;
+	struct tagItem* next;
+};
+typedef struct tagItem Item;
+
+int getCount();
+void delItem();
+void printList();
+int findItem(void* s);
+void addItem(void* s);
---
+++ my/labs/day1_1/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+
+
+int main(int argc, char *argv[]) {
+	signed long int n1;
+	long n2;
+	
+	signed char ch;
+	unsigned char ch1;
+	
+	double a,b=7,c=2;
+	a=b/c;
+	
+	int K1=5;
+	long long K2=3LL;
+	double k3=.4;
+	
+	 
+	
+	return 0;
+}
